cab431c3d3ffcaa7da9a25b6914337b8f9053416
JuggerInavitable <ivanmyasnikov1@gmail.com>
2023-11-29 18:00:05 +0300

    HomeTrying

---
+++ js/slider.js
@@ -0,0 +1,36 @@
+let slider = document.querySelector('.slider');
+let prevButton = document.querySelector('.slider__button-prv');
+let nextButton = document.querySelector('.slider__button-nxt');
+let slides = Array.from(slider.querySelectorAll('img'));
+let slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
